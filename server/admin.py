4d0a6394fe46abba9e40fb7b4b0a63fa3d23c97f
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2023-04-04 13:47:53 +0100

    make some changes

--- server/admin.py
+++ server/admin.py
@@ -17,8 +17,8 @@ class Mainframe:
         self.master = master
         self.master.title("UKULELE - Admin")
         self.master.geometry('1350x680+5+5')
-        self.master.iconimage = PhotoImage(file='pic\icon_img.png')
-        self.master.iconphoto(True, self.master.iconimage)
+        # self.master.iconimage = PhotoImage(file='pic\icon_img.png')
+        # self.master.iconphoto(True, self.master.iconimage)
         self.master.minsize(100, 100)
         self.master.resizable(0, 0)
 
